e6672413b9a99de7eda0dc44e8d5ab241ec8726c
Anthony Te <112227149+AnthonyJTe@users.noreply.github.com>
2024-10-12 02:28:11 +0000

    still working on functions

--- functions.py
+++ functions.py
@@ -17,34 +17,55 @@ def rmperson(person, id):
             for item in receipt["items"]:
 
             receipt["group"].remove(person)
-            
+
 def removePersonItem(person, id, itemID):
     with open('receipt_data.json', 'r+') as file:
         data = json.load(file)
-    redistributtion(person, id, itemID)
-    data["receipts"][id]["item"][itemID]["people"].remove(person)
+    redistributtion(person, id, itemID, data, false)
+    data["receipts"][id]["items"][itemID]["people"].remove(person)
 
-
-def redistribution(person, id, itemID):
+def addPersonItem(person, id, itemID):
     with open('receipt_data.json', 'r+') as file:
         data = json.load(file)
-    group = []
-    price = data["receipts"][id]["item"][itemID]["price"]
-    for people in data["receipts"][id]["item"][itemID]["people"]:
-        group.append(person)
+    redistributtion(person, id, itemID, data)
+    data["receipts"][id]["items"][itemID]["people"].append(person)
+
+
+def redistribution(person, id, itemID, data):
+    group =  data["receipts"][id]["items"][itemID]["people"][:]
+    price = data["receipts"][id]["items"][itemID]["price"]
     for people in data["receipts"][id]["group"]:
         for peopleR in group:
             if people["name"] == peopleR:
                 people["owed"] =- price/group.lens()
                 break
-    group.remove(person)
+    if person in group:
+        group.remove(person)
+    else: 
+        group.append()
     for people in data["receipts"][id]["group"]:
         for peopleR in group:
             if people["name"] == peopleR:
                 people["owed"] =+ price/group.lens()
                 break
         
+def changeItem(id, itemID, newName, newPrice):
+    with open('receipt_data.json', 'r+') as file:
+        data = json.load(file)
 
+def priceAdjust(id, itemID, oldPrice, newPrice, data):
+    for people in data["receipts"][id]["group"]:
+        for peopleR in group:
+            if people["name"] == peopleR:
+                people["owed"] =+ oldPrice/group.lens()
+                break
+    for people in data["receipts"][id]["group"]:
+        for peopleR in group:
+            if people["name"] == peopleR:
+                people["owed"] =+ newPrice/group.lens()
+                break
+    data["receipts"][id]["total"] =- oldPrice
+    data["receipts"][id]["total"] =+ newPrice
 
 # add receipt
 # remove person
